9672776693edfee17675e4d9bf47987d9b21861b
José Manuel Mora <josema81@hotmail.com>
2016-03-20 19:12:31 -0600

    Actualización del proyecto, desplegado gráfico de la semana 2

--- ConsultaISBN/ViewController.swift
+++ ConsultaISBN/ViewController.swift
@@ -18,14 +18,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    @IBOutlet weak var txtResult: UITextView!
     @IBOutlet weak var txtIsbn: UITextField!
+    @IBOutlet weak var lblTitulo: UILabel!
+    @IBOutlet weak var lblAutores: UILabel!
+    @IBOutlet weak var imgPortada: UIImageView!
     
     @IBAction func btnClear(sender: AnyObject) {
         txtIsbn.text=""
-        txtResult.text=""
+        lblAutores.text=""
+        lblTitulo.text=""
+        imgPortada.image = nil;
     }
-
 
     @IBAction func IsbnSearch(sender: UITextField) {
         txtIsbn.resignFirstResponder()  //if desired
@@ -54,20 +57,56 @@ class ViewController: UIViewController, UITextFieldDelegate {
         var error = ""
         
         if (datos != nil){
+            /*
             let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
             txtResult.text = String(texto)
+            */
+            
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dicJson = json as! NSDictionary
+                
+                if let dicISBN = dicJson["ISBN:\(txtIsbn.text!)"] as! NSDictionary! {
+                    
+                    lblTitulo.text = dicISBN["title"] as! NSString as String
+                    
+                    let dicAutores = dicISBN["authors"] as! NSArray
+                    var autores = ""
+                    
+                    for autor in dicAutores {
+                        autores += "- \(autor["name"]!!)\n"
+                    }
+                    lblAutores.text = autores
+                    
+                    
+                    
+                    imgPortada.image = nil
+                    
+                    if let dicCover = dicISBN["cover"] as! NSDictionary! {
+                        if let url  = NSURL(string: dicCover["medium"] as! NSString as String), data = NSData(contentsOfURL: url) {
+                            imgPortada.image = UIImage(data: data)
+                        }
+                    }
+                    
+                    
+                }
+            } catch _
+            {
+                print("Ocurrió un error al obtener la información")
+            }
 
         }
         else
         {
             error = "No hay conexión a Internet"
-            txtResult.text = error
+            lblTitulo.text = error
 
         }
         
+        txtIsbn.resignFirstResponder()
         //print(texto)
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
